91c48e8627567b1c97671781ac035ca6e63522e7
RafaelFelic <rafaelfelic@gmail.com>
2023-04-12 16:40:00 +1000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6a0e7758-d751-458e-bc38-23f2a8f76115';
+export const MODAL_CLOSE_SEC = 2.5;
